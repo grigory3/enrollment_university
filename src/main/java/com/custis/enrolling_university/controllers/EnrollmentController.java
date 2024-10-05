bb10fb1aff029f0d087b12970f830fd71c551711
grigory <grigory@enrolling-university.com>
2024-10-05 16:42:22 +0300

    an error has been resolved where a student could re-enroll in a course

--- src/main/java/com/custis/enrolling_university/controllers/EnrollmentController.java
+++ src/main/java/com/custis/enrolling_university/controllers/EnrollmentController.java
@@ -4,6 +4,9 @@ import com.custis.enrolling_university.models.Course;
 import com.custis.enrolling_university.service.EnrollmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +21,11 @@ public class EnrollmentController {
     @PostMapping
     public String enroll(@RequestParam Long studentId, @RequestParam Long courseId) {
         return enrollmentService.enrollStudent(studentId, courseId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handlerDataIntegrityViolationException (DataIntegrityViolationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Студент уже записан на данный курс!");
     }
 
     @GetMapping("/courses")
