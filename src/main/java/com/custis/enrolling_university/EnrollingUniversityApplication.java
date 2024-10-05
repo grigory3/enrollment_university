0d1c9da4d400ac847804211ef6d20ff9afef2737
grigory <grigory@enrolling-university.com>
2024-10-05 12:59:51 +0300

    init commit

---
+++ src/main/java/com/custis/enrolling_university/EnrollingUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.custis.enrolling_university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollingUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollingUniversityApplication.class, args);
+	}
+
+}
